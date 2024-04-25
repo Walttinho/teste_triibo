979800a3cccadf6dec55319b8a4d62d5c5024233
Walttinho <walter.netto@live.com>
2024-04-25 09:11:33 -0300

    feat (test): user delete by ID test

---
+++ test/user/delete.spec.js
@@ -0,0 +1,42 @@
+const request = require("supertest");
+const app = require("../../src/app");
+const knex = require("../../src/database/connection");
+
+describe("DELETE /user/:id", () => {
+  let userIdOne;
+
+  beforeAll(async () => {
+    const [id1] = await knex("users").insert(
+      {
+        name: "Walter Netto",
+        email: "walter@example.com",
+        password: "123456",
+      },
+      ["id"]
+    );
+
+    userIdOne = id1;
+  });
+
+  afterAll(async () => {
+    await knex("users").whereIn("email", ["walter@example.com"]).del();
+  });
+
+  it("should delete user by ID", async () => {
+    const response = await request(app).delete(`/user/${userIdOne}`);
+    expect(response.status).toBe(204);
+    expect(response.body).toEqual({});
+  });
+
+  it("should return error message for non-existing user", async () => {
+    const response = await request(app).delete("/user/999999");
+    expect(response.status).toBe(404);
+    expect(response.body.error).toBe("User not found");
+  });
+
+  it("should return error message for invalid ID format", async () => {
+    const response = await request(app).delete("/user/string");
+    expect(response.status).toBe(400);
+    expect(response.body.error).toBe("Invalid ID format");
+  });
+});

ced98b499868105cc2bd430b73ff76f289aba6ab
Walttinho <walter.netto@live.com>
2024-04-25 21:10:13 -0300

    fix (test): update updateUser by ID

--- test/user/put.spec.js
+++ test/user/put.spec.js
@@ -6,7 +6,7 @@ describe("PUT /user/:id", () => {
   let userIdOne;
 
   beforeAll(async () => {
-    const [{id: id1}] = await knex("users").insert(
+    const [{ id: id1 }] = await knex("users").insert(
       {
         name: "Walter Netto",
         email: "walter@example.com",
@@ -19,7 +19,7 @@ describe("PUT /user/:id", () => {
   });
 
   afterAll(async () => {
-    await knex("users").whereIn("email", ["walter@example.com"]).del();
+    await knex("users").whereIn("email", ["renata.update@example.com"]).del();
   });
 
   it("should update user by ID", async () => {
@@ -39,10 +39,58 @@ describe("PUT /user/:id", () => {
     expect(response.body.id).toBe(userIdOne);
   });
 
+  it("should update only the email", async () => {
+    const updatedUserData = {
+      email: "renata.update@example.com"     
+    };
+
+    const response = await request(app)
+      .put(`/user/${userIdOne}`)
+      .send(updatedUserData);
+
+    expect(response.status).toBe(200);
+    expect(response.body.email).toBe(updatedUserData.email);
+    expect(response.body).toHaveProperty("name");
+    expect(response.body).toHaveProperty("id");
+  });
+
+  it("should update only the password", async () => {
+    const updatedUserData = {
+      password: "654321",
+    };
+
+    const response = await request(app)
+      .put(`/user/${userIdOne}`)
+      .send(updatedUserData);
+
+    expect(response.status).toBe(200);
+    expect(response.body.password).toBe(updatedUserData.password);
+    expect(response.body).toHaveProperty("name");
+    expect(response.body).toHaveProperty("email");
+    expect(response.body).toHaveProperty("id");
+  });
+
+  it("should update only the name", async () => {
+    const updatedUserData = {
+      name: "Renata Medeiros Update",
+    };
+
+    const response = await request(app)
+      .put(`/user/${userIdOne}`)
+      .send(updatedUserData);
+
+    expect(response.status).toBe(200);
+    expect(response.body.name).toBe(updatedUserData.name);
+    expect(response.body).toHaveProperty("email");
+    expect(response.body).toHaveProperty("password");
+    expect(response.body).toHaveProperty("id");
+  });
+
+
   it("should return  error message for empty request body", async () => {
     const response = await request(app).put(`/user/${userIdOne}`).send({});
     expect(response.status).toBe(400);
-    expect(response.body.error).toBe("Request body cannot be empty");
+    expect(response.body.message).toBe("Request body cannot be empty");
   });
 
   it("should return error message for non-existing user", async () => {
@@ -53,7 +101,7 @@ describe("PUT /user/:id", () => {
     });
 
     expect(response.status).toBe(404);
-    expect(response.body.error).toBe("User not found");
+    expect(response.body.message).toBe("User not found");
   });
 
   it("should return error message for invalid ID format", async () => {
@@ -64,6 +112,17 @@ describe("PUT /user/:id", () => {
     });
 
     expect(response.status).toBe(400);
-    expect(response.body.error).toBe("Invalid ID format");
+    expect(response.body.message).toBe("Invalid ID format");
+  });
+
+  it("should return error message for invalid email", async () => {
+    const response = await request(app).put(`/user/${userIdOne}`).send({
+      name: "Renata Medeiros",
+      email: "invalid-email",
+      password: "654321",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body.message).toBe("Invalid email");
   });
 });

e59b6ced2aede5d562d01a976c96e1a340948cd1
walttinho <walter.netto@live.com>
2024-04-27 22:18:54 -0300

    docs (swagger): add docs with swagger

--- src/router/indexRouter.js
+++ src/router/indexRouter.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
+const swaggerRouter = require("./swaggerRouter");
 const userRouter = require("./userRouter");
 
 router.use("/user", userRouter);
+
+router.use("/docs", swaggerRouter);
 
 module.exports = router;

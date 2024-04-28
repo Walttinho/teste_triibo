d10bc2f739eff5baa7c537e07808385b6b8ad053
walttinho <walter.netto@live.com>
2024-04-27 22:55:03 -0300

    chore (cors): install cors

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = require("./router/indexRouter");
 require("dotenv").config();
 require("./database/connection");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: "*" }));
 
 app.use(router);
 
